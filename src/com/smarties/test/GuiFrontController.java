cbb6fec4dde1719d1415c3abaa92664b468574bd
Mahmoud Cheikh <mahmoud.cheikh@esprit.tn>
2022-04-20 13:46:06 +0100

    mail

--- src/com/smarties/test/GuiFrontController.java
+++ src/com/smarties/test/GuiFrontController.java
@@ -5,9 +5,15 @@
  */
 package com.smarties.test;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
 
 /**
  * FXML Controller class
@@ -16,12 +22,26 @@ import javafx.fxml.Initializable;
  */
 public class GuiFrontController implements Initializable {
 
+    @FXML
+    private Button btnlogout;
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    }
+
+    @FXML
+    private void logout(ActionEvent event) {
+         FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
+        try {
+            Parent root = loader.load();
+            btnlogout.getScene().setRoot(root);
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 
 }
