093294277c7e8faff20f4f2cb0f2cbc23ce1a60e
Nacib Mariem <mariem.nacib@esprit.tn>
2022-05-12 21:37:34 +0200

    template mail stock & emplacement
    NACIB MARIEM

--- src/com/smarties/services/EmplacementService.java
+++ src/com/smarties/services/EmplacementService.java
@@ -39,7 +39,9 @@ public class EmplacementService {
     public EmplacementService() {
         cnx = MaConnexion.getInstance().getCnx();
     }
+// mail template
 
+// end mail template
     public void ajouterEmplacement(Emplacement s) {
         String query = "insert into emplacement(lieu,capacite,stock_id) values(?,?,?)";
         try {
@@ -175,7 +177,69 @@ public class EmplacementService {
             message.setFrom(new InternetAddress(MyAccountEmail));
             message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
             message.setSubject("Les Sites De çaRoule ! ");
-            message.setText("Merci pour Votre demande des information , \n 1) le site  est : "+lieu+" \n 2) La Capacité de ce Site est : "+cap+" \n 3) Vous pouvez Consulter la map : https://www.google.com/maps/place/"+lieu+"/ \n Merci Pour Votre confiance ...");
+            message.setContent("<body style=\"background-color:black\">\n" +
+"    <table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\"\n" +
+"           width=\"550\" bgcolor=\"white\" style=\"border:2px solid black\">\n" +
+"        <tbody>\n" +
+"            <tr>\n" +
+"                <td align=\"center\">\n" +
+"                    <table align=\"center\" border=\"0\" cellpadding=\"0\" \n" +
+"                           cellspacing=\"0\" class=\"col-550\" width=\"550\">\n" +
+"                        <tbody>\n" +
+"                            <tr>\n" +
+"                                <td align=\"center\" style=\"background-color:#ff7f50;\n" +
+"                                           height: 50px;\">\n" +
+"  \n" +
+"                                    <a href=\"#\" style=\"text-decoration: none;\">\n" +
+"                                        <p style=\"color:white;\n" +
+"                                                  font-weight:bold;\">\n" +
+"                                            Ca Roule\n" +
+"                                        </p>\n" +
+"                                    </a>\n" +
+"                                </td>\n" +
+"                            </tr>\n" +
+"                        </tbody>\n" +
+"                    </table>\n" +
+"                </td>\n" +
+"            </tr>\n" +      
+"            <tr style=\"height: 300px;\">\n" +
+"                <td align=\"center\" style=\"border: none;\n" +
+"                           border-bottom: 2px solid #4cb96b; \n" +
+"                           padding-right: 20px;padding-left:20px\">\n" +
+"  \n" +
+"                    <p style=\"font-weight: bolder;font-size: 42px;\n" +
+"                              letter-spacing: 0.025em;\n" +
+"                              color:black;\">\n" +
+"                        Hello !\n" +
+"                        <br> Ça Roule application Desktop ! \n" +
+"                    </p>\n" +
+"                </td>\n" +
+"            </tr>\n" +
+"  \n" +
+"            <tr style=\"display: inline-block;\">\n" +
+"                <td style=\"height: 150px;\n" +
+"                           padding: 20px;\n" +
+"                           border: none; \n" +
+"                           border-bottom: 2px solid #361B0E;\n" +
+"                           background-color: white;\">\n" +
+"                    \n" +
+"                    <h2 class=\"data\" \n" +
+"                       style=\"text-align: justify-all;\n" +
+"                              align-items: center; \n" +
+"                              font-size: 15px;\n" +
+"                              padding-bottom: 12px;\">\n" +
+"                     Merci pour Votre demande des information ,<br><br>"+
+                    "1) le site  est : "+lieu+" \n <br>"+
+                            "2) La Capacité de ce Site est : "+cap+" \n<br>"+
+                                    " 3) Vous pouvez Consulter la map : https://www.google.com/maps/place/"+lieu+"/ \n<br>"+
+                                             " Merci Pour Votre confiance ... <br>\n\n" +
+"                    </h2>\n" +                  
+"                </td>\n" +
+"            </tr>\n" +
+"        </tbody>\n" +
+"    </table>\n" +
+"</body>","text/html");
+            //message.setText("1) le site  est : "+lieu+" \n 2) La Capacité de ce Site est : "+cap+" \n 3) Vous pouvez Consulter la map : https://www.google.com/maps/place/"+lieu+"/ \n Merci Pour Votre confiance ...");
             return message;
         } catch (Exception ex) {
             Logger.getLogger(AbonnementService.class.getName()).log(Level.SEVERE, null, ex);
--- src/com/smarties/services/StockService.java
+++ src/com/smarties/services/StockService.java
@@ -58,7 +58,66 @@ public class StockService {
     public StockService() {
         cnx = MaConnexion.getInstance().getCnx();
     }
-
+// mail template
+    private String text="<body style=\"background-color:black\">\n" +
+"    <table align=\"center\" border=\"0\" cellpadding=\"0\" cellspacing=\"0\"\n" +
+"           width=\"550\" bgcolor=\"white\" style=\"border:2px solid black\">\n" +
+"        <tbody>\n" +
+"            <tr>\n" +
+"                <td align=\"center\">\n" +
+"                    <table align=\"center\" border=\"0\" cellpadding=\"0\" \n" +
+"                           cellspacing=\"0\" class=\"col-550\" width=\"550\">\n" +
+"                        <tbody>\n" +
+"                            <tr>\n" +
+"                                <td align=\"center\" style=\"background-color:#ff7f50;\n" +
+"                                           height: 50px;\">\n" +
+"  \n" +
+"                                    <a href=\"#\" style=\"text-decoration: none;\">\n" +
+"                                        <p style=\"color:white;\n" +
+"                                                  font-weight:bold;\">\n" +
+"                                            Ca Roule\n" +
+"                                        </p>\n" +
+"                                    </a>\n" +
+"                                </td>\n" +
+"                            </tr>\n" +
+"                        </tbody>\n" +
+"                    </table>\n" +
+"                </td>\n" +
+"            </tr>\n" +      
+"            <tr style=\"height: 300px;\">\n" +
+"                <td align=\"center\" style=\"border: none;\n" +
+"                           border-bottom: 2px solid #4cb96b; \n" +
+"                           padding-right: 20px;padding-left:20px\">\n" +
+"  \n" +
+"                    <p style=\"font-weight: bolder;font-size: 42px;\n" +
+"                              letter-spacing: 0.025em;\n" +
+"                              color:black;\">\n" +
+"                        Hello !\n" +
+"                        <br> Ça Roule application Desktop ! \n" +
+"                    </p>\n" +
+"                </td>\n" +
+"            </tr>\n" +
+"  \n" +
+"            <tr style=\"display: inline-block;\">\n" +
+"                <td style=\"height: 150px;\n" +
+"                           padding: 20px;\n" +
+"                           border: none; \n" +
+"                           border-bottom: 2px solid #361B0E;\n" +
+"                           background-color: white;\">\n" +
+"                    \n" +
+"                    <h2 class=\"data\" \n" +
+"                       style=\"text-align: justify-all;\n" +
+"                              align-items: center; \n" +
+"                              font-size: 15px;\n" +
+"                              padding-bottom: 12px;\">\n" +
+"                      Hey Admine , \n Stock Limité Svp Contacter Votre Fournisseur ! pour plus de details sur l'état du stock consulter la section stock dans l'application \n" +
+"                    </h2>\n" +                  
+"                </td>\n" +
+"            </tr>\n" +
+"        </tbody>\n" +
+"    </table>\n" +
+"</body>";
+// end mail template
     public void ajouterStock(Stock s) {
         String query = "insert into stock(libelle,id_produit_id,prix,quantite,disponibilite) values(?,?,?,?,?)";
         try {
@@ -289,7 +348,8 @@ public class StockService {
             message.setFrom(new InternetAddress(MyAccountEmail));
             message.setRecipient(Message.RecipientType.TO, new InternetAddress(recipient));
             message.setSubject("Ca Roule Commande ");
-            message.setText("Hey Admine , \n Stock Limite Svp Contacter Votre Fournisseur ! ");
+          //  message.setText("Hey Admine , \n Stock Limite Svp Contacter Votre Fournisseur ! ");
+            message.setContent(text,"text/html");
             return message;
         } catch (Exception ex) {
             Logger.getLogger(AbonnementService.class.getName()).log(Level.SEVERE, null, ex);
